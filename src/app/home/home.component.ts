4e49b8786df1d088258b05b7510c2de53184061d
mrashutoshkrsingh <mrashutoshkrsingh@gmail.com>
2020-03-08 12:14:25 +0530

    login logout button conditional rendering based on isloggedin from service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit, Injectable, OnChanges, DoCheck, AfterContentChecked } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -8,15 +8,24 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./home.component.css']
 })
 @Injectable()
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,  DoCheck {
+  
+  // ngAfterContentChecked(): void {
+  //   console.log(this.auth,'after')
+  // }
 
-  isLoggedIn:false;
+  isLoggedIn=false;
 
   constructor(private route:Router, private auth:AuthService) { }
 
   ngOnInit() {
-    
+    //console.log(this.auth,"Init")
   }
+
+  ngDoCheck(): void {
+    this.isLoggedIn=this.auth.loggedIn;
+  }
+ 
 
   onLoadServers(){
     this.route.navigate(['/servers'], {queryParams:{allowEdit:'1'}, fragment:'loading'})
